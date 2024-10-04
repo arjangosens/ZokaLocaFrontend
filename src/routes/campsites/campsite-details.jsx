f1ff6f45523efa73803a51e198569f1a403d9562
Arjan Gosens <a.gosens@student.fontys.nl>
2024-10-04 11:00:54 +0200

    Finished campsite details page

--- src/routes/campsites/campsite-details.jsx
+++ src/routes/campsites/campsite-details.jsx
@@ -4,6 +4,18 @@ import {Link, useLoaderData} from "react-router-dom";
 import CampsiteIcon from "../../components/campsites/campsite-icon.jsx";
 import PersonLimit from "../../components/campsites/person-limit.jsx";
 import CampsitePrice from "../../components/campsites/campsite-price.jsx";
+import CampsiteAmenity from "../../components/campsites/campsite-amenity.jsx";
+
+function getAddressString(address) {
+    let result = "?";
+    if (address?.street && address?.houseNumber) {
+        result = `${address.street} ${address.houseNumber}`;
+    } else if (address.street) {
+        result = address.street;
+    }
+
+    return result;
+}
 
 export async function loader({params}) {
     try {
@@ -39,25 +51,106 @@ export default function CampsiteDetails() {
                 </div>
             </div>
             <div className="row">
-                <div className="col-12 col-md-8">
+                <div className="col-12 col-lg-8">
                     <img className="img-fluid" alt="campsite thumbnail" src="https://placehold.co/1920x1080"/>
                 </div>
-                <div className="col-12 col-md-4">
-                    <div className="row d-block d-md-none">
+                <div className="col-12 col-lg-4">
+                    <div className="row d-block d-lg-none mt-4">
                         <h2 className="text-center">Praktisch</h2>
                         <hr/>
                     </div>
                     <div className="row">
-                        <div className="col-6 col-md-12">
+                        {/*Address*/}
+                        <div className="col-12 col-sm-6 col-lg-12 mb-4">
                             <div className="card text-bg-light ">
                                 <div className="card-header">
                                     <i className="fa-solid fa-city"></i> Adres
                                 </div>
-                                <div className="card-body">
-
-                                    <p>Woonplaats: {campsite.address.city}</p>
-                                    <p>Land: {campsite.address.country}</p>
+                                <div className="card-body d-flex flex-column">
+                                    <span><b>Adres:</b> {getAddressString(campsite.address)}</span>
+                                    <span><b>Postcode:</b> {campsite.address.zipcode ?? "?"}</span>
+                                    <span><b>Woonplaats:</b> {campsite.address.city ?? "?"}</span>
+                                    <span><b>Land:</b> {campsite.address.country ?? "?"}</span>
                                 </div>
+                            </div>
+                        </div>
+                        {/*Arrival and departure times*/}
+                        <div className="col-12 col-sm-6 col-lg-12 mb-4">
+                            <div className="card text-bg-light ">
+                                <div className="card-header">
+                                    <i className="fa-solid fa-clock"></i> Aankomst- en vertrektijden
+                                </div>
+                                <div className="card-body d-flex flex-column">
+                                    <span><b><i
+                                        className="fa-solid fa-right-to-bracket"></i> Aankomst:</b> {campsite.arrivalTime ?? "?"}</span>
+                                    <span><b> <i
+                                        className="fa-solid fa-right-from-bracket"></i> Vertrek:</b> {campsite.departureTime ?? "?"}</span>
+                                </div>
+                            </div>
+                        </div>
+                        {/*External sources*/}
+                        {campsite?.externalSources != null &&
+                            <div className="col-12 mb-4">
+                                <div className="card text-bg-light ">
+                                    <div className="card-header">
+                                        <i className="fa-solid fa-globe"></i> Externe bronnen
+                                    </div>
+                                    <div className="card-body d-flex flex-column">
+                                        {Object.keys(campsite.externalSources).map((key) => (
+                                            <span key={key}><b>{key.charAt(0).toUpperCase() + key.slice(1)}:</b> <a
+                                                href={campsite.externalSources[key]} target="_blank"
+                                                rel="noopener noreferrer">{campsite.externalSources[key]}</a></span>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-lg-8 text-center">
+                    <h2 className="mt-4">Beschrijving</h2>
+                    <hr/>
+                    <p>{campsite.description ?? <span className="text-body-tertiary">Geen beschrijving beschikbaar</span>}</p>
+                </div>
+                <div className="col-12 col-lg-4">
+                    <h2 className="text-center mt-4">Voorzieningen</h2>
+                    <hr/>
+                    <div className="card text-bg-light mb-4">
+                        <div className="card-header">
+                            Algemeen
+                        </div>
+                        <div className="card-body d-flex flex-column">
+                            <CampsiteAmenity icon={"fa-solid fa-toilet"} prefix={"Toiletten"}
+                                             amount={campsite.numOfToilets}/>
+                            <CampsiteAmenity icon={"fa-solid fa-shower"} prefix={"Douches"}
+                                             amount={campsite.numOfShowers}/>
+                            <CampsiteAmenity icon={"fa-solid fa-faucet-drip"} prefix={"Waterpunten"}
+                                             amount={campsite.numOfWaterSources}/>
+                            {campsite.campsiteType === "BUILDING" && <>
+                                <CampsiteAmenity icon={"fa-solid fa-door-closed"} prefix={"Lokalen"}
+                                                 amount={campsite.numOfRooms}/>
+                                <CampsiteAmenity icon={"fa-solid fa-door-open"} prefix={"Gedeelde ruimtes"}
+                                                 amount={campsite.numOfCommonAreas}/></>
+                            }
+                            {campsite.campsiteType === "FIELD" &&
+                                <CampsiteAmenity icon={"fa-solid fa-maximize"} prefix={"Grootte"}
+                                                 amount={campsite.sizeSquareMeters}
+                                                 unit={"m²"}/>}
+                        </div>
+                    </div>
+
+                    <div className="card text-bg-light mb-4">
+                        <div className="card-header">
+                            Overig
+                        </div>
+                        <div className="card-body p-0">
+                            <div className="d-flex flex-wrap p-2">
+                                {campsite.facilities.map((facility) => (
+                                    <small key={facility.id}
+                                           className="badge rounded-pill text-bg-secondary me-2 mb-2">{facility.name}</small>
+                                ))}
                             </div>
                         </div>
                     </div>
