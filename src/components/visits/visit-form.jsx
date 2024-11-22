124464c1e7c9e8f5f96cb30abc982cc4fda719da
Arjan Gosens <a.gosens@student.fontys.nl>
2024-11-22 12:19:16 +0100

    Added page for adding a new visit to a campsite

--- src/components/visits/visit-form.jsx
+++ src/components/visits/visit-form.jsx
@@ -3,19 +3,21 @@ import {useAuth} from "../../providers/auth-provider.jsx";
 import PropTypes from "prop-types";
 import RequiredMark from "../shared/required-mark.jsx";
 
-export default function VisitForm({visit, onSubmit}) {
+export default function VisitForm({visit, isSubmitProcessing, onSubmit}) {
     const {loggedInUser} = useAuth();
-    const {register, handleSubmit, formState: {errors}, watch} = useForm({
+    const {register, handleSubmit, formState: {errors}, setValue, watch} = useForm({
         mode: "onBlur"
     });
 
     const onSubmitHandler = (data) => {
+        data.pros = data.pros.split("\n").filter(Boolean);
+        data.cons = data.cons.split("\n").filter(Boolean);
         console.log("Submit visit: ", data);
         onSubmit(data);
     }
 
     return (
-        <form onSubmit={onSubmit(onSubmitHandler)}>
+        <form onSubmit={handleSubmit(onSubmitHandler)}>
             {/* Branch select */}
             <div className="mb-3">
                 <label>Speltak<RequiredMark/></label>
@@ -23,6 +25,7 @@ export default function VisitForm({visit, onSubmit}) {
                     className={"form-select" + (errors.branchId ? " is-invalid" : "")}
                     {...register("branchId", {required: "Speltak is vereist"})}
                     defaultValue={visit?.branchId}
+                    disabled={isSubmitProcessing}
                 >
                     {loggedInUser.branches.map(branch => (
                         <option key={branch.id} value={branch.id}>{branch.name}</option>
@@ -45,6 +48,7 @@ export default function VisitForm({visit, onSubmit}) {
                                 className={"form-control" + (errors.arrivalDate ? " is-invalid" : "")}
                                 {...register("arrivalDate", {required: "Aankomstdatum is vereist"})}
                                 defaultValue={visit?.arrivalDate}
+                                disabled={isSubmitProcessing}
                             />
                             {errors.arrivalDate && <div className="invalid-feedback">{errors.arrivalDate.message}</div>}
                         </div>
@@ -55,6 +59,7 @@ export default function VisitForm({visit, onSubmit}) {
                                 className={"form-control" + (errors.departureDate ? " is-invalid" : "")}
                                 {...register("departureDate", {required: "Vertrekdatum is vereist"})}
                                 defaultValue={visit?.departureDate}
+                                disabled={isSubmitProcessing}
                             />
                             {errors.departureDate &&
                                 <div className="invalid-feedback">{errors.departureDate.message}</div>}
@@ -63,20 +68,110 @@ export default function VisitForm({visit, onSubmit}) {
                 </div>
             </div>
 
+            {/* Rating */}
+            <div className="mb-3">
+                <label>Hoe beoordeel je het verblijf?<RequiredMark /> <small
+                    className="text-info">(Op een schaal van 1 - 10)</small></label>
+                <input
+                    type="number"
+                    className={"form-control" + (errors.rating ? " is-invalid" : "")}
+                    {...register("rating", {
+                        min: {value: 1, message: "Beoordeling kan niet lager zijn dan 1"},
+                        max: {value: 10, message: "Beoordeling kan niet hoger zijn dan 10"},
+                        required: "Beoordeling is vereist"
+                    })}
+                    defaultValue={visit?.rating}
+                    disabled={isSubmitProcessing}
+                />
+                {errors.rating && <div className="invalid-feedback">{errors.rating.message}</div>}
+            </div>
+
             {/* Number of people */}
+            <div className="mb-3">
+                <label>Met hoeveel personen was je?</label>
+                <div className="input-group">
+                    <span className="input-group-text"><i className="fa-solid fa-users"></i></span>
+                    <input
+                        type="number"
+                        placeholder={"?"}
+                        className={"form-control" + (errors.numberOfPeople ? " is-invalid" : "")}
+                        {...register("numberOfPeople", {min: {value: 1, message: "Aantal kan niet lager zijn dan 1"}})}
+                        defaultValue={visit?.numOfPeople}
+                        disabled={isSubmitProcessing}
+                    />
+                    {errors.numberOfPeople && <div className="invalid-feedback">{errors.numberOfPeople.message}</div>}
+                </div>
+            </div>
 
             {/* Price */}
+            <label>Hoe duur was het verblijf?</label>
+            <div className="input-group mb-3">
+                <span className="input-group-text">€</span>
+                <input
+                    className={"form-control" + (errors.price ? " is-invalid" : "")}
+                    placeholder="?"
+                    type="number"
+                    step="0.01"
+                    {...register("price")}
+                    defaultValue={visit?.price}
+                    disabled={isSubmitProcessing}
+                />
+            </div>
 
             {/* Pros and cons */}
+            <div className="card mb-3">
+                <div className="card-header">Plus- en minpunten</div>
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-12 col-md-6 mb-3 mb-md-0">
+                            <label>Pluspunten <small className={"text-info"}>(Eén per regel)</small></label>
+                            <textarea
+                                className="form-control"
+                                rows="3"
+                                {...register("pros")}
+                                defaultValue={visit?.pros?.join("\n")}
+                                disabled={isSubmitProcessing}
+                            />
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <label>Minpunten <small className={"text-info"}>(Eén per regel)</small></label>
+                            <textarea
+                                className="form-control"
+                                rows="3"
+                                {...register("cons")}
+                                defaultValue={visit?.cons?.join("\n")}
+                                disabled={isSubmitProcessing}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             {/* Description */}
+            <div className="mb-3">
+                <label>Opmerkingen</label>
+                <textarea
+                    className="form-control"
+                    rows="5"
+                    {...register("description")}
+                    defaultValue={visit?.description}
+                    disabled={isSubmitProcessing}
+                />
+            </div>
 
             {/* Submit button */}
+            <button className="btn btn-primary mb-5" type="submit" disabled={isSubmitProcessing}>
+                {isSubmitProcessing && (<div className="spinner-border spinner-border-sm" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>)}
+                Opslaan
+            </button>
         </form>
     )
 }
 
 VisitForm.propTypes = {
     visit: PropTypes.object,
-    onSubmit: PropTypes.func.isRequired
+    onSubmit: PropTypes.func.isRequired,
+    isSubmitProcessing: PropTypes.bool.isRequired
 }
--- src/routes/campsites/add-visit.jsx
+++ src/routes/campsites/add-visit.jsx
@@ -1,12 +1,34 @@
-import {useLoaderData} from "react-router-dom";
+import {useLoaderData, useNavigate} from "react-router-dom";
 import CampsiteIcon from "../../components/campsites/campsite-icon.jsx";
 import VisitForm from "../../components/visits/visit-form.jsx";
+import {backendApi} from "../../utils/backend-api.jsx";
+import {useState} from "react";
 
 export default function AddVisit() {
     const {campsite} = useLoaderData();
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [isSubmitProcessing, setIsSubmitProcessing] = useState(false);
+    const navigate = useNavigate();
 
-    const handleSubmit = (data) => {
-        console.log("Submit visit: ", data);
+    const handleSubmit = async (data) => {
+        setIsSubmitProcessing(true);
+        setErrorMsg(null);
+
+        const visit = {
+            ...data,
+            campsiteId: campsite.id
+        };
+
+        try {
+            const response = await backendApi.post("/visits", visit);
+            console.log("Visit added: ", response.data);
+            navigate(`/campsites/${campsite.id}`);
+        } catch (error) {
+            console.error("Add visit failed: ", error);
+            setErrorMsg(error.message);
+        } finally {
+            setIsSubmitProcessing(false);
+        }
     };
 
     return (
@@ -15,7 +37,8 @@ export default function AddVisit() {
             <h1 className="page-header-margin text-center">Bezoek toevoegen</h1>
             <h2 className="text-center"><CampsiteIcon campsiteType={campsite.campsiteType}/> {campsite.name}</h2>
             <hr/>
-            <VisitForm onSubmit={handleSubmit}/>
+            {errorMsg && <div className="alert alert-danger">{errorMsg}</div>}
+            <VisitForm isSubmitProcessing={isSubmitProcessing} onSubmit={handleSubmit}/>
         </div>
         </div>);
 }
