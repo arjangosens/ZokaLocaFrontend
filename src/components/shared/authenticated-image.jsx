76a9bd862527da2343c68c6cfbecea37a9f5fbf4
Arjan Gosens <a.gosens@student.fontys.nl>
2024-11-29 10:59:24 +0100

    Added AuthenticatedImage component for fetching images from the backend

---
+++ src/components/shared/authenticated-image.jsx
@@ -0,0 +1,39 @@
+import {useEffect, useState} from "react";
+import PropTypes from "prop-types";
+import {backendApi} from "../../utils/backend-api.jsx";
+
+export default function AuthenticatedImage({imageId, placeholder, alt, className}) {
+    const [imageSrc, setImageSrc] = useState(placeholder);
+
+    useEffect(() => {
+        const fetchImage = async () => {
+            if (imageId) {
+                try {
+                    const response = await backendApi.get(`/assets/${imageId}/download`, {
+                        responseType: 'blob'
+                    });
+                    const objectURL = URL.createObjectURL(response.data);
+                    setImageSrc(objectURL);
+                } catch (error) {
+                    console.error("Error fetching image:", error);
+                }
+            }
+        };
+
+        fetchImage();
+    }, [imageId]);
+
+    return <img className={className} src={imageSrc} alt={alt} />;
+}
+
+AuthenticatedImage.propTypes = {
+    imageId: PropTypes.string,
+    placeholder: PropTypes.string,
+    alt: PropTypes.string.isRequired,
+    className: PropTypes.string
+};
+
+AuthenticatedImage.defaultProps = {
+    placeholder: "https://placehold.co/1920x1080",
+    className: ""
+};
